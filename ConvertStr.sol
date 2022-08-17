deaa3014bd1352bf641d56eff83c3d9cffe04013
Oluscosco <89050580+Oluscosco@users.noreply.github.com>
2022-08-17 23:31:44 +0100

    Convert case in solidity
    convert string to lower case in solidity

---
+++ ConvertStr.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+contract toLowerThingy {
+    function _toLower(string memory str) external pure returns (string memory) {
+        bytes memory bStr = bytes(str);
+        bytes memory bLower = new bytes(bStr.length);
+        for (uint i = 0; i < bStr.length; i++) {
+            // Uppercase character
+            if ((uint8(bStr[i]) >= 65) && (uint8(bStr[i]) <= 90)) {
+                // So we add 32 to make it lowercase
+                bLower[i] = bytes1(uint8(bStr[i]) + 32);
+            } else {
+                bLower[i] = bStr[i];
+            }
+        }
+        return string(bLower);
+    }
+}
