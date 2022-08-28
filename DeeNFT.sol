0fe539544bec95d5934d469c9a254c0021ec1b51
Oluscosco <89050580+Oluscosco@users.noreply.github.com>
2022-08-28 12:11:15 +0100

    A simple NFT solidity code
    the NFT was implemented using OpenZeppelin 
    the token counter to keep track of the NFT's, safemint was to prevent someone minting ERC721 to a contract which does not support ERC721 transfer.

---
+++ DeeNFT.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.6;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract DeeCollectible is ERC721 {
+    uint256 public tokenCounter;
+    constructor () public ERC721 ("Dee", "DEE"){
+        tokenCounter = 0;
+    }
+
+    function createDeeCollectible(string memory tokenURI) public returns (uint256) {
+        uint256 newItemId = tokenCounter;
+        _safeMint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        tokenCounter = tokenCounter + 1;
+        return newItemId;
+    }
+
+}
+
